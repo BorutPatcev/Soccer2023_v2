a7d3981f9e160417c0f6a21174466c6a9e6701a8
Borut Patcev <borut.patcev@gmail.com>
2023-02-19 18:25:52 +0100

    Added includes and defines

--- Soccer2023_Robot1_V2.ino
+++ Soccer2023_Robot1_V2.ino
@@ -1,3 +1,22 @@
+#include <Wire.h>
+#include <IMUBoschBNO055.h>
+#include <VL53L1X.h>
+
+#define Bluetooth Serial7
+#define Camera Serial8
+
+#ifndef toRad
+#define toRad(x) ((x) / 180.0 * PI)
+#endif
+
+IMUBoschBNO055 compass;
+VL53L1X lidar;
+
+int lid[8] = {0,0,0,0,0,0,0,0};
+int lin[32];
+bool swc[3];
+bool but[4];
+
 void setup() {
   // put your setup code here, to run once:
 
@@ -6,4 +25,9 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+void error(String message) {
+  Serial.println(message);
+  Bluetooth.println(message);
 }
